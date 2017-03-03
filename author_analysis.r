71c9f202579213fc692f70f75d55d6099d5bab45
Rob Briers <r.briers@napier.ac.uk>
2017-03-03 11:31:38 +0000

    updating plot to use poisson GLM and simplifying prediction code

--- author_analysis.r
+++ author_analysis.r
@@ -8,23 +8,22 @@ library(viridis)
 url<-'https://raw.githubusercontent.com/robbriers/authors/master/coauthors.csv'
 auths<-read.csv(url, header=TRUE)
 
-# plot the data to look at the trend
+# plot the data to look at the trend, using poisson GLM for fit
 p1 <- ggplot(auths, aes(x = year, y = authors, colour = authors)) +
   geom_point(size=1.5) +
   scale_color_viridis(end = 0.80) +
   labs(title = "Change in number of co-authors over time",
        x = "year of publication",
        y = "number of co-authors") +
-  geom_smooth(method = "lm", se = TRUE) +
+  geom_smooth(method = "glm", method.args = list(family = "poisson"), se = TRUE) +
   theme_bw() +
   theme(legend.position="none")
 
 p1
 
-
-# glm analysis of pattern of change; count data so use poisson family
+# fit a poisson glm to the pattern of change, so we can make some predictions
 auth.m1<-glm(authors~year, data=auths, family="poisson")
 summary(auth.m1)
 
-# so how many co-authors will I have in 2050?
-exp(predict(auth.m1, data.frame(year=c(2050))))
+# so how many co-authors will I have in 2040?
+exp(predict(auth.m1, data.frame(year=2040)))
